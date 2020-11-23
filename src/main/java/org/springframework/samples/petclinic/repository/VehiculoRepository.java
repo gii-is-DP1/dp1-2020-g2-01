a161c95960a066a4e18644f0d3413367add1ef29
Sergio <sergiderakes@gmail.com>
2020-11-23 09:51:18 +0100

    Update y delete cita

--- src/main/java/org/springframework/samples/petclinic/repository/VehiculoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/VehiculoRepository.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.petclinic.repository;
 
+import java.util.List;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -10,7 +12,7 @@ public interface VehiculoRepository extends CrudRepository<Vehiculo, Integer>  {
 	
 	Vehiculo save(Vehiculo vehiculo) throws DataAccessException;
 	
-	Iterable<Vehiculo> findAll() throws DataAccessException;
+	List<Vehiculo> findAll() throws DataAccessException;
 
 	@Query("SELECT DISTINCT vehiculo FROM Vehiculo vehiculo WHERE vehiculo.matricula LIKE :matricula")
 	Vehiculo findVehiculoMatricula(@Param("matricula") String matricula) throws DataAccessException;
--- src/main/java/org/springframework/samples/petclinic/service/VehiculoService.java
+++ src/main/java/org/springframework/samples/petclinic/service/VehiculoService.java
@@ -1,9 +1,9 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Vehiculo;
 import org.springframework.samples.petclinic.repository.VehiculoRepository;
@@ -26,7 +26,7 @@ public class VehiculoService {
 	}
 	
 	@Transactional(readOnly = true)
-	public Iterable<Vehiculo> findAll() throws DataAccessException {
+	public List<Vehiculo> findAll() throws DataAccessException {
 		return vehiculoRepository.findAll();
 	}
 	
