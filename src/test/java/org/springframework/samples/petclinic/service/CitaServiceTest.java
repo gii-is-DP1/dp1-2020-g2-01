1f95ef5b906051b3664958187552a3175f9b95c6
serarirud <serarirud@alum.us.es>
2020-11-27 16:17:50 +0100

    Cita delete test

--- src/test/java/org/springframework/samples/petclinic/service/CitaServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/CitaServiceTest.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
@@ -117,6 +118,31 @@ class CitaServiceTest {
 		citaService.saveCita(c);
 		
 		assertFalse(this.citaService.findCitaById(2).isPresent());
+	}
+	
+	@Test
+	void shouldDeleteCita() {
+		Cita c = new Cita();
+		
+		c.setFecha(LocalDate.now().plusDays(1));
+		c.setHora(10);
+		c.setTipoCita(TipoCita.AIRE_ACONDICIONADO);
+		
+		Vehiculo v = new Vehiculo();
+		
+		v.setMatricula("1111AAA");
+		v.setModelo("Seat Ibiza");
+		v.setNumBastidor("1");
+		vehiculoService.saveVehiculo(v);
+		
+		c.setVehiculo(vehiculoService.findVehiculoByMatricula("1111AAA"));
+		citaService.saveCita(c);
+		
+		assertEquals(c, citaService.findCitaByFechaAndHora(LocalDate.now().plusDays(1), 10));
+		
+		citaService.delete(citaService.findCitaByFechaAndHora(LocalDate.now().plusDays(1), 10));
+		
+		assertNull(citaService.findCitaByFechaAndHora(LocalDate.now().plusDays(1), 10));
 	}
 
 }
