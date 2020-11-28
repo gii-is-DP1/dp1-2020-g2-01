c0474a092dd67449d7e00170e7543186e13ff6dd
Alejandro <alemorgar4@alum.us.es>
2020-11-28 21:05:42 +0100

    Pruebas empleados y añadir delete a prueba de vehiculo

--- src/main/java/org/springframework/samples/petclinic/repository/EmpleadoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/EmpleadoRepository.java
@@ -1,6 +1,16 @@
 package org.springframework.samples.petclinic.repository;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.model.Empleado;
 
-public interface EmpleadoRepository extends CrudRepository<Empleado, Integer> {}
+public interface EmpleadoRepository extends CrudRepository<Empleado, Integer> {
+	
+	Empleado save(Empleado empleado) throws DataAccessException;
+	
+	@Query("SELECT DISTINCT empleado FROM Empleado empleado WHERE empleado.dni LIKE :dni")
+	Empleado findEmpleadoDNI(@Param("dni") String dni) throws DataAccessException;
+	
+}
---
+++ src/test/java/org/springframework/samples/petclinic/service/EmpleadoServiceTest.java
@@ -0,0 +1,135 @@
+package org.springframework.samples.petclinic.service;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.model.Empleado;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class EmpleadoServiceTest {
+
+	@Autowired
+	protected EmpleadoService empleadoService;
+	
+	@Test
+	void shouldInsertEmpleado() {
+		Empleado e = new Empleado();
+		
+		e.setApellidos("Ramirez Perez");
+		e.setCorreo("laurita@gmail.com");
+		e.setDni("36283951R");
+		e.setFecha_fin_contrato(LocalDate.now().plusYears(10));
+		e.setFecha_ini_contrato(LocalDate.now().minusYears(2));
+		e.setFechaNacimiento(LocalDate.now().minusYears(40));
+		e.setNombre("Laura");
+		e.setNum_seg_social("82938103-23183-21");
+		e.setSueldo(1098L);
+		e.setTelefono(678456736);
+		
+		empleadoService.saveEmpleado(e);
+		assertEquals(e, empleadoService.findEmpleadoDni("36283951R"));
+	}
+	
+	@Test
+	void shouldNotInsertEmpleadoInvalido() {
+		Empleado e = new Empleado();
+		
+		e.setApellidos("Ramirez Perez");
+		e.setCorreo("laurita@gmail.com");
+		e.setDni("");
+		e.setFecha_fin_contrato(LocalDate.now().plusYears(10));
+		e.setFecha_ini_contrato(LocalDate.now().minusYears(2));
+		e.setFechaNacimiento(LocalDate.now().minusYears(40));
+		e.setNombre("Laura");
+		e.setNum_seg_social("82938103-23183-21");
+		e.setSueldo(1098L);
+		e.setTelefono(678456736);
+		
+		assertThrows(ConstraintViolationException.class, () -> empleadoService.saveEmpleado(e));
+	}
+	
+	@Test
+	void shouldUpdateEmpleado() {
+		Empleado e = new Empleado();
+		
+		e.setApellidos("Ramirez Perez");
+		e.setCorreo("laurita@gmail.com");
+		e.setDni("36283951R");
+		e.setFecha_fin_contrato(LocalDate.now().plusYears(10));
+		e.setFecha_ini_contrato(LocalDate.now().minusYears(2));
+		e.setFechaNacimiento(LocalDate.now().minusYears(40));
+		e.setNombre("Laura");
+		e.setNum_seg_social("82938103-23183-21");
+		e.setSueldo(1098L);
+		e.setTelefono(678456736);
+		
+		empleadoService.saveEmpleado(e);
+		
+		Empleado e1 = empleadoService.findEmpleadoDni("36283951R");
+		e1.setSueldo(1180L);
+		
+		empleadoService.saveEmpleado(e1);
+		
+		assertEquals(e, empleadoService.findEmpleadoDni("36283951R"));
+	}
+	
+	@Test
+	void shouldNotUpdateEmpleadoInvalido() {
+		Empleado e = new Empleado();
+		
+		e.setApellidos("Ramirez Perez");
+		e.setCorreo("laurita@gmail.com");
+		e.setDni("36283951R");
+		e.setFecha_fin_contrato(LocalDate.now().plusYears(10));
+		e.setFecha_ini_contrato(LocalDate.now().minusYears(2));
+		e.setFechaNacimiento(LocalDate.now().minusYears(40));
+		e.setNombre("Laura");
+		e.setNum_seg_social("82938103-23183-21");
+		e.setSueldo(1098L);
+		e.setTelefono(678456736);
+		
+		empleadoService.saveEmpleado(e);
+		
+		Empleado e1 = empleadoService.findEmpleadoDni("36283951R");
+		e1.setDni("");
+		empleadoService.saveEmpleado(e1);
+		
+		assertFalse(this.empleadoService.findById(e1.getId()).isPresent());
+		
+	}
+	
+	@Test 
+	void shoulDeleteEmplead() {
+		Empleado e = new Empleado();
+		
+		e.setApellidos("Ramirez Perez");
+		e.setCorreo("laurita@gmail.com");
+		e.setDni("36283951R");
+		e.setFecha_fin_contrato(LocalDate.now().plusYears(10));
+		e.setFecha_ini_contrato(LocalDate.now().minusYears(2));
+		e.setFechaNacimiento(LocalDate.now().minusYears(40));
+		e.setNombre("Laura");
+		e.setNum_seg_social("82938103-23183-21");
+		e.setSueldo(1098L);
+		e.setTelefono(678456736);
+		
+		empleadoService.saveEmpleado(e);
+		assertEquals(e, empleadoService.findEmpleadoDni("36283951R"));
+		
+		empleadoService.delete(e);
+		
+		assertNull(empleadoService.findEmpleadoDni("36283951R"));
+
+	}
+}
