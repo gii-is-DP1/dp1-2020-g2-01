5fbd41dbb482d6a06db999647f310d20546a1881
jesvarzam <jesvarzam@alum.us.es>
2020-11-29 11:06:48 +0100

    empleado

--- src/main/java/org/springframework/samples/petclinic/service/EmpleadoService.java
+++ src/main/java/org/springframework/samples/petclinic/service/EmpleadoService.java
@@ -6,11 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Empleado;
-<<<<<<< Upstream, based on branch 'jesvarzam' of https://github.com/gii-is-DP1/dp1-2020-g2-01.git
 import org.springframework.samples.petclinic.model.Vehiculo;
-=======
 import org.springframework.samples.petclinic.model.User;
->>>>>>> 5615ec4 tipocita
 import org.springframework.samples.petclinic.repository.EmpleadoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,13 +25,9 @@ public class EmpleadoService {
 	private AuthoritiesService authService;
 	
 	@Transactional
-<<<<<<< Upstream, based on branch 'jesvarzam' of https://github.com/gii-is-DP1/dp1-2020-g2-01.git
-	public void saveEmpleado(Empleado empleado) throws DataAccessException{
-=======
 	public void saveEmpleado(Empleado empleado) {
 		
 		empleado.getUsuario().setAuthorities(new ArrayList<>());
->>>>>>> 5615ec4 tipocita
 		empleadoRepository.save(empleado);
 		userService.saveUser(empleado.getUsuario());
 		authService.saveAuthorities(empleado.getUsuario().getUsername(), "empleado");
