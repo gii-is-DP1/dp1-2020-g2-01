64ed4aa23679e22ac8484fe392543312d80d34d8
serarirud <serarirud@alum.us.es>
2020-12-20 14:16:08 +0100

    Siguen los errores

--- src/main/java/org/springframework/samples/petclinic/service/SendEmailService.java
+++ src/main/java/org/springframework/samples/petclinic/service/SendEmailService.java
@@ -1,30 +1,48 @@
 package org.springframework.samples.petclinic.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-@Service
-public class SendEmailService {
+@Service("mailManager")
+public class SendEmailService{
 	
 	 @Autowired
-	 private JavaMailSender mailSender;
+	 public JavaMailSender mailSender;
 	 
 //	 public void setMailSender(MailSender mailSender){
 //			this.mailSender = mailSender;
 //	}
 	 
-	 public void sendEmail(String to, String subject, String content) {
-
-		SimpleMailMessage email = new SimpleMailMessage();
-
-//		email.setFrom("tallersevillacustoms@gmail.com");
-        email.setTo(to);
-        email.setSubject(subject);
-        email.setText(content);
-
-        mailSender.send(email);
+	 public Boolean sendEmail(String to, String subject, String content) {
+//
+//		SimpleMailMessage email = new SimpleMailMessage();
+//
+////		email.setFrom("tallersevillacustoms@gmail.com");
+//        email.setTo(to);
+//        email.setSubject(subject);
+//        email.setText(content);
+//
+//        mailSender.send(email);
+		 Boolean sent = false;
+		 
+		 MimeMessage message = mailSender.createMimeMessage();
+		 MimeMessageHelper helper = new MimeMessageHelper(message);
+		 
+		 try {
+			 helper.setTo(to);
+			 helper.setText(content, true);
+			 helper.setSubject(subject);
+			 mailSender.send(message);
+			 sent = true;
+		 }catch(Exception e) {
+			 e.printStackTrace();
+		 }
+		 
+		 return sent;
     }
 
 
