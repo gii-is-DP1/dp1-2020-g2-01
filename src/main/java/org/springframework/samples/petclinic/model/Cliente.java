3dfd0fd3d7b8708414002eae6320e4a44f8b4893
Sergio Arias Ruda <72388113+serarirud@users.noreply.github.com>
2020-12-29 11:04:54 +0100

    Relación entre cliente y factura no es bidireccional

--- src/main/java/org/springframework/samples/petclinic/model/Cliente.java
+++ src/main/java/org/springframework/samples/petclinic/model/Cliente.java
@@ -9,9 +9,11 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name="clientes")
 public class Cliente extends Persona{
@@ -23,7 +25,7 @@ public class Cliente extends Persona{
 	@JoinColumn(name = "username", referencedColumnName = "username")
 	private User user;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany
 	private List<Factura> facturas;
 
 
