a2acc96e8c8b21fc49a0d78e471f007267a5c979
Francisco-Borrego <fraborcar@alum.us.es>
2021-01-07 18:02:43 +0100

    Prueba para RN1 añadida

--- src/test/java/org/springframework/samples/petclinic/service/ReparacionServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/ReparacionServiceTest.java
@@ -64,6 +64,9 @@ class ReparacionServiceTest {
 	protected ClienteService clienteService;
 	
 	
+	
+	
+	
 	@Test
 	void shouldInsertReparacion() throws DataAccessException, FechasReparacionException, Max3ReparacionesSimultaneasPorEmpleadoException, DuplicatedMatriculaException {
 		
@@ -133,9 +136,7 @@ class ReparacionServiceTest {
 		
 		
 	}
-	//
-	//HAY QUE HACER TEST DE MAX3REPARACIONESSIMULTANEAS
-	//
+	
 	
 	@Test
 	@Transactional
@@ -278,6 +279,158 @@ class ReparacionServiceTest {
 		assertThrows(FechasReparacionException.class, () -> this.reparacionService.saveReparacion(r));
 		
 	}
+	
+	
+	@Test
+	void shouldNotInsertReparacionConEmpleadoCon3ReparacionesSimultaneas() throws DataAccessException, FechasReparacionException, Max3ReparacionesSimultaneasPorEmpleadoException, DuplicatedMatriculaException {
+		//Setup inicial de reparacion (faltan cosas)
+		Reparacion r = new Reparacion();
+		r.setDescripcion("Descripción de prueba"); 
+		r.setFechaEntrega(LocalDate.now().plusDays(7));
+		r.setTiempoEstimado(LocalDate.now().plusDays(8));
+		r.setFechaFinalizacion(LocalDate.now().plusDays(9));
+		r.setFechaRecogida(LocalDate.now().plusDays(10));
+	
+		//Setup de reparacion y cita
+		Cita c = new Cita();
+		TipoCita t = tipoCitaService.findById(1).get();
+		List<TipoCita> tipos = new ArrayList<>();
+		tipos.add(t);
+		Vehiculo v = new Vehiculo();
+		TipoVehiculo tv = tipoVehiculoService.findById(1).get();
+		v.setMatricula("4052DMR");
+		v.setModelo("Renault Clio 2006");
+		v.setNumBastidor("VSSZZZ6KZ1R149943");
+		v.setTipoVehiculo(tv);
+		c.setFecha(LocalDate.now().plusDays(2));
+		c.setHora(18);
+		c.setTiposCita(tipos);
+		vehiculoService.saveVehiculo(v);
+		c.setVehiculo(vehiculoService.findVehiculoByMatricula("4052DMR").get());
+		
+		//Setup de taller
+		Taller taller = new Taller();
+		taller.setCorreo("test@test.com");
+		taller.setName("test");
+		taller.setTelefono("123456789");
+		taller.setUbicacion("calle test");
+		
+		tallerService.saveTaller(taller);
+		
+		c.setTaller(taller);
+		
+		citaService.saveCita(c);
+		
+		//Asigna una cita a la reparacion
+		r.setCita(citaService.findCitaByFechaAndHora(LocalDate.now().plusDays(2), 18));
+		
+		//Añadir un empleado
+		Empleado e1 = new Empleado();
+		List<Empleado> empleados = new ArrayList<>();
+		User userP = new User();
+		userP.setUsername("nombreusuario");
+		userP.setPassword("passdeprueba");
+		userP.setEnabled(true);
+		e1.setNombre("Pepito");
+		e1.setApellidos("Grillo");
+		e1.setDni("89898988A");
+		e1.setFechaNacimiento(LocalDate.now().minusYears(20));
+		e1.setFecha_ini_contrato(LocalDate.now().minusDays(10));
+		e1.setFecha_fin_contrato(LocalDate.now().plusYears(1));
+		e1.setSueldo(1000L);
+		e1.setUsuario(userP);
+		e1.setNum_seg_social("1");
+		e1.setEmail("prueba@prueba.com");
+		e1.setTelefono("777777777");
+		
+		empleados.add(e1);
+
+		e1.setTaller(taller);
+		
+		empleadoService.saveEmpleado(e1);
+
+		//Crear 3 reparaciones y asociarlas al empleado. Las reparaciones tienen mismo taller y vehiculo pero citas distintas
+		
+		//Reparacion 1
+		Reparacion r1 = new Reparacion();
+		r1.setDescripcion("Descripción de prueba"); 
+		r1.setFechaEntrega(LocalDate.now().plusDays(7));
+		r1.setTiempoEstimado(LocalDate.now().plusDays(8));
+		r1.setFechaFinalizacion(LocalDate.now().plusDays(9));
+		r1.setFechaRecogida(LocalDate.now().plusDays(10));
+	
+		//CUIDADO CON PONER MISMA FECHA Y HORA QUE EL RESTO DE CITAS
+		Cita c1 = new Cita();
+		c1.setFecha(LocalDate.now().plusDays(2));
+		c1.setHora(10);
+		c1.setTiposCita(tipos);
+		c1.setVehiculo(vehiculoService.findVehiculoByMatricula("4052DMR").get());
+		c1.setTaller(taller);
+		
+		citaService.saveCita(c1);
+		
+		r1.setCita(citaService.findCitaByFechaAndHora(LocalDate.now().plusDays(2), 10));
+		
+		r1.setEmpleados(empleados);
+		
+		reparacionService.saveReparacion(r1);
+		
+		//Reparacion 2
+		Reparacion r2 = new Reparacion();
+		r2.setDescripcion("Descripción de prueba"); 
+		r2.setFechaEntrega(LocalDate.now().plusDays(7));
+		r2.setTiempoEstimado(LocalDate.now().plusDays(8));
+		r2.setFechaFinalizacion(LocalDate.now().plusDays(9));
+		r2.setFechaRecogida(LocalDate.now().plusDays(10));
+	
+	
+		Cita c2 = new Cita();
+		c2.setFecha(LocalDate.now().plusDays(2));
+		c2.setHora(11);
+		c2.setTiposCita(tipos);
+		c2.setVehiculo(vehiculoService.findVehiculoByMatricula("4052DMR").get());
+		c2.setTaller(taller);
+		
+		citaService.saveCita(c2);
+		
+		r2.setCita(citaService.findCitaByFechaAndHora(LocalDate.now().plusDays(2), 11));
+		
+		
+		r2.setEmpleados(empleados);
+		
+		reparacionService.saveReparacion(r2);
+		
+		
+		//Reparacion 3
+		Reparacion r3 = new Reparacion();
+		r3.setDescripcion("Descripción de prueba"); 
+		r3.setFechaEntrega(LocalDate.now().plusDays(7));
+		r3.setTiempoEstimado(LocalDate.now().plusDays(8));
+		r3.setFechaFinalizacion(LocalDate.now().plusDays(9));
+		r3.setFechaRecogida(LocalDate.now().plusDays(10));
+	
+	
+		Cita c3 = new Cita();
+		c3.setFecha(LocalDate.now().plusDays(2));
+		c3.setHora(12);
+		c3.setTiposCita(tipos);
+		c3.setVehiculo(vehiculoService.findVehiculoByMatricula("4052DMR").get());
+		c3.setTaller(taller);
+		
+		citaService.saveCita(c3);
+		
+		r3.setCita(citaService.findCitaByFechaAndHora(LocalDate.now().plusDays(2), 12));
+		
+		r3.setEmpleados(empleados);
+		
+		reparacionService.saveReparacion(r3);
+		
+		
+		//Comprueba que se lanza la regla de negocio
+		r.setEmpleados(empleados);
+		assertThrows(Max3ReparacionesSimultaneasPorEmpleadoException.class, () -> this.reparacionService.saveReparacion(r));
+	}
+	
 	
 	@Test
 	void shouldUpdateReparacion() throws DataAccessException, FechasReparacionException, Max3ReparacionesSimultaneasPorEmpleadoException, DuplicatedMatriculaException {
