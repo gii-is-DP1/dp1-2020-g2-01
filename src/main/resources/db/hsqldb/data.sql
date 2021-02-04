0f10fa711ab64d54d462e559e976cb5cc2e4f43c
serarirud <serarirud@alum.us.es>
2021-02-04 13:24:56 +0100

    Cambio de modelado de datos

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,3 +1,4 @@
+  
 -- One admin user, named admin1 with passwor 4dm1n and authority admin
 --INSERT INTO users(username,password,enabled) VALUES ('admin1','4dm1n',TRUE);
 --INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
@@ -223,6 +224,4 @@ INSERT INTO solicitudes(terminada, cantidad, recambio_id, empleado_id, reparacio
 
 
 -- 
-
-
 
