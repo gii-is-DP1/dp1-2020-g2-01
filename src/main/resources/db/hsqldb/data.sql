5b7b31686b2573467e7b0bf726b1c9622c6ca026
Francisco-Borrego <fraborcar@alum.us.es>
2021-02-04 18:21:57 +0100

    merge

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,4 +1,3 @@
-  
 -- One admin user, named admin1 with passwor 4dm1n and authority admin
 --INSERT INTO users(username,password,enabled) VALUES ('admin1','4dm1n',TRUE);
 --INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
@@ -207,6 +206,7 @@ INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, f
 <<<<<<< Upstream, based on origin/master
 <<<<<<< Upstream, based on origin/master
 <<<<<<< Upstream, based on origin/master
+<<<<<<< Upstream, based on origin/master
 
 =======
 <<<<<<< Upstream, based on origin/fraborcar
@@ -221,120 +221,18 @@ INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, f
 >>>>>>> 6bd1d6a RN5 realizada
 =======
 >>>>>>> f2060c7 merge
+=======
+>>>>>>> ead78aa merge
 INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
 		VALUES (2,'Descripción', '2020-10-24', '2020-10-22', '2020-10-22', '2020-10-23', 201);
 INSERT INTO facturas(id, fecha_Pago) VALUES (1, '2020-12-27');
 INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 100, 4,1);
-=======
-<<<<<<< Upstream, based on origin/fraborcar
-<<<<<<< Upstream, based on origin/fraborcar
-<<<<<<< Upstream, based on origin/fraborcar
-<<<<<<< Upstream, based on origin/fraborcar
-=======
-<<<<<<< Upstream, based on origin/master
->>>>>>> c767211 RN5 realizada
-INSERT INTO facturas(id, fecha_Pago, descuento) VALUES (1, '2020-12-27', 15);
-INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recambio_id, cantidad,factura) VALUES (360, 15, 1, 'Cambio 4 ruedas', 100, 4,1);
-<<<<<<< Upstream, based on origin/master
->>>>>>> 081bb00 RN5 realizada
-=======
-=======
-=======
-<<<<<<< Upstream, based on origin/master
->>>>>>> 1b96e9a Merge
-=======
->>>>>>> 629ca62 merge
-INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
-		VALUES (2,'Descripción', '2020-10-24', '2020-10-22', '2020-10-22', '2020-10-23', 201);
-INSERT INTO facturas(id, fecha_Pago) VALUES (1, '2020-12-27');
-INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 100, 4,1);
-<<<<<<< Upstream, based on origin/master
-<<<<<<< Upstream, based on origin/fraborcar
->>>>>>> 44dd997 Añadido horas al pdf factura
-<<<<<<< Upstream, based on origin/master
->>>>>>> 0b75705 Añadido horas al pdf factura
-=======
-=======
-=======
-
-
-INSERT INTO facturas(id, fecha_Pago) VALUES (1, '2020-12-27');
-INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recambio_id, cantidad,factura) VALUES (360, 15, 1, 'Cambio 4 ruedas', 100, 4,1);
->>>>>>> 9e1deb7 Merge
->>>>>>> 1b96e9a Merge
->>>>>>> 9f5e1df Merge
-=======
->>>>>>> 629ca62 merge
 
 INSERT INTO horas_trabajadas(id, trabajo_realizado, precio_hora, horas_trabajadas, empleado) VALUES (10, 'Cambio de rueda', 10.5, 3, 100);
 INSERT INTO horas_trabajadas(id, trabajo_realizado, precio_hora, horas_trabajadas, empleado) VALUES (11, 'Cambio de frenos', 10.5, 4, 101);
 
-<<<<<<< Upstream, based on origin/master
-<<<<<<< Upstream, based on origin/master
-<<<<<<< Upstream, based on origin/master
 INSERT INTO horas_reparacion VALUES (2, 10);
 INSERT INTO horas_reparacion VALUES (2, 11);
-=======
-=======
-<<<<<<< Upstream, based on origin/fraborcar
-<<<<<<< Upstream, based on origin/master
->>>>>>> 0b75705 Añadido horas al pdf factura
-=======
-<<<<<<< Upstream, based on origin/fraborcar
->>>>>>> 9f5e1df Merge
-INSERT INTO horas_reparacion VALUES (1, 10);
-INSERT INTO horas_reparacion VALUES (1, 11);
-=======
-INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
-		VALUES (2,'Descripción', '2020-10-24', '2020-12-23', '2020-10-10', '2020-12-25', 201);
-INSERT INTO facturas(id, fecha_Pago) VALUES (1, '2020-12-27');
-INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, ejemplar_recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 1, 4,1);
-INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, cantidad,factura) VALUES (80, 0, 2,'2h de trabajo',1,1);
-INSERT INTO facturas(id, fecha_Pago) VALUES (2, '2020-12-25');
-INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, ejemplar_recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 1, 4,2);
-INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, cantidad,factura) VALUES (80, 0, 2,'2h de trabajo',1,2);
-INSERT INTO reparaciones_empleados(REPARACION_ID, EMPLEADOS_ID) VALUES (1, 100);
-INSERT INTO reparaciones_empleados(REPARACION_ID, EMPLEADOS_ID) VALUES (1, 101);
->>>>>>> 21df115 RN5 realizada
-<<<<<<< Upstream, based on origin/master
->>>>>>> 081bb00 RN5 realizada
-=======
-=======
-=======
-<<<<<<< Upstream, based on origin/master
->>>>>>> 1b96e9a Merge
-=======
->>>>>>> 629ca62 merge
-INSERT INTO horas_reparacion VALUES (2, 10);
-INSERT INTO horas_reparacion VALUES (2, 11);
-<<<<<<< Upstream, based on origin/master
-<<<<<<< Upstream, based on origin/fraborcar
->>>>>>> 44dd997 Añadido horas al pdf factura
-<<<<<<< Upstream, based on origin/master
->>>>>>> 0b75705 Añadido horas al pdf factura
-=======
-=======
-=======
-INSERT INTO horas_reparacion VALUES (1, 10);
-INSERT INTO horas_reparacion VALUES (1, 11);
-
-INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
-		VALUES (2,'Descripción', '2020-10-24', '2020-12-23', '2020-10-10', '2020-12-25', 201);
-		
---INSERT INTO facturas(id, fecha_Pago) VALUES (1, '2020-12-27');
-
---INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, ejemplar_recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 1, 4,1);
---INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, cantidad,factura) VALUES (80, 0, 2,'2h de trabajo',1,1);
---INSERT INTO facturas(id, fecha_Pago) VALUES (2, '2020-12-25');
---INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, ejemplar_recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 1, 4,2);
---INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, cantidad,factura) VALUES (80, 0, 2,'2h de trabajo',1,2);
---INSERT INTO reparaciones_empleados(REPARACION_ID, EMPLEADOS_ID) VALUES (1, 100);
---INSERT INTO reparaciones_empleados(REPARACION_ID, EMPLEADOS_ID) VALUES (1, 101);
->>>>>>> 9e1deb7 Merge
->>>>>>> 1b96e9a Merge
->>>>>>> 9f5e1df Merge
-=======
->>>>>>> 629ca62 merge
 
 =======
 <<<<<<< Upstream, based on origin/fraborcar
@@ -452,6 +350,5 @@ INSERT INTO solicitudes(terminada, cantidad, recambio_id, empleado_id, reparacio
 
 
 -- 
-
 
 
