0b757056d5dfdecbac202371387c16581f9e2ecf
serarirud <serarirud@alum.us.es>
2021-02-04 17:36:30 +0100

    Añadido horas al pdf factura

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -209,15 +209,23 @@ INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, f
 		VALUES (1,'Descripción', '2021-10-24', null, '2021-10-22', '2021-10-23', 200);
 <<<<<<< Upstream, based on origin/fraborcar
 <<<<<<< Upstream, based on origin/fraborcar
+<<<<<<< Upstream, based on origin/fraborcar
 =======
 <<<<<<< Upstream, based on origin/master
 >>>>>>> c767211 RN5 realizada
 INSERT INTO facturas(id, fecha_Pago, descuento) VALUES (1, '2020-12-27', 15);
 INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recambio_id, cantidad,factura) VALUES (360, 15, 1, 'Cambio 4 ruedas', 100, 4,1);
+=======
+INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
+		VALUES (2,'Descripción', '2020-10-24', '2020-10-22', '2020-10-22', '2020-10-23', 201);
+INSERT INTO facturas(id, fecha_Pago) VALUES (1, '2020-12-27');
+INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 100, 4,1);
+>>>>>>> 44dd997 Añadido horas al pdf factura
 
 INSERT INTO horas_trabajadas(id, trabajo_realizado, precio_hora, horas_trabajadas, empleado) VALUES (10, 'Cambio de rueda', 10.5, 3, 100);
 INSERT INTO horas_trabajadas(id, trabajo_realizado, precio_hora, horas_trabajadas, empleado) VALUES (11, 'Cambio de frenos', 10.5, 4, 101);
 
+<<<<<<< Upstream, based on origin/fraborcar
 INSERT INTO horas_reparacion VALUES (1, 10);
 INSERT INTO horas_reparacion VALUES (1, 11);
 =======
@@ -232,6 +240,10 @@ INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, canti
 INSERT INTO reparaciones_empleados(REPARACION_ID, EMPLEADOS_ID) VALUES (1, 100);
 INSERT INTO reparaciones_empleados(REPARACION_ID, EMPLEADOS_ID) VALUES (1, 101);
 >>>>>>> 21df115 RN5 realizada
+=======
+INSERT INTO horas_reparacion VALUES (2, 10);
+INSERT INTO horas_reparacion VALUES (2, 11);
+>>>>>>> 44dd997 Añadido horas al pdf factura
 
 INSERT INTO reparacionescomunes(id, nombre, descripcion) VALUES (1, 'Reparación espejo retrovisor izquierdo', 'Po lo hase así y asá');
 INSERT INTO reparacionescomunes(id, nombre, descripcion) VALUES (2, 'Reparación luneta térmica', 'Po lo hase así y con la mano asá');
