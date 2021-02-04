68fc1feebc81aaf973f18956198184ba4343a8cd
Francisco-Borrego <fraborcar@alum.us.es>
2021-02-04 18:10:22 +0100

    merge

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -11,16 +11,12 @@
 
 /*INSERT INTO clientes(username,password,enabled) VALUES ('fraborcar','prueba',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (4,'fraborcar','owner');
-
 INSERT INTO clientes(username,password,enabled) VALUES ('serarirud','prueba',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (5,'serarirud','owner');
-
 INSERT INTO clientes(username,password,enabled) VALUES ('alemorgar4','prueba',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (6,'alemorgar4','owner');
-
 INSERT INTO clientes(username,password,enabled) VALUES ('antfunmej','prueba',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (7,'antfunmej','owner');
-
 INSERT INTO clientes(username,password,enabled) VALUES ('jesvarzam','prueba',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (8,'jesvarzam','owner');*/
 
@@ -28,13 +24,11 @@ INSERT INTO authorities(id,username,authority) VALUES (8,'jesvarzam','owner');*/
 /*INSERT INTO specialties VALUES (1, 'radiology');
 INSERT INTO specialties VALUES (2, 'surgery');
 INSERT INTO specialties VALUES (3, 'dentistry');
-
 INSERT INTO vet_specialties VALUES (2, 1);
 INSERT INTO vet_specialties VALUES (3, 2);
 INSERT INTO vet_specialties VALUES (3, 3);
 INSERT INTO vet_specialties VALUES (4, 2);
 INSERT INTO vet_specialties VALUES (5, 1);
-
 INSERT INTO types VALUES (1, 'cat');
 INSERT INTO types VALUES (2, 'dog');
 INSERT INTO types VALUES (3, 'lizard');
@@ -72,14 +66,11 @@ INSERT INTO types VALUES (6, 'hamster');*/
 --INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '2010-03-09', 5, 9);
 /*INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
-
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Matias', '2012-06-08', 1, 11);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'Mathew', '2012-07-08', 1, 12);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (16, 'Hallo', '2012-07-08', 1, 13);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (17, 'Guapo', '2012-06-08', 1, 14);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (18, 'EteSech', '2012-06-08', 1, 15);
-
-
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
@@ -208,6 +199,7 @@ INSERT INTO citas_tipocita VALUES (201, 1);
 INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
 		VALUES (1,'Descripción', '2021-10-24', null, '2021-10-22', '2021-10-23', 200);
 <<<<<<< Upstream, based on origin/master
+<<<<<<< Upstream, based on origin/master
 
 INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
 		VALUES (2,'Descripción', '2020-10-24', '2020-10-22', '2020-10-22', '2020-10-23', 201);
@@ -238,10 +230,13 @@ INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recam
 =======
 <<<<<<< Upstream, based on origin/master
 >>>>>>> 1b96e9a Merge
+=======
+>>>>>>> 629ca62 merge
 INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, fecha_entrega, fecha_recogida, cita_id) 
 		VALUES (2,'Descripción', '2020-10-24', '2020-10-22', '2020-10-22', '2020-10-23', 201);
 INSERT INTO facturas(id, fecha_Pago) VALUES (1, '2020-12-27');
 INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recambio_id, cantidad,factura) VALUES (360, 15, 2, 'Cambio 4 ruedas', 100, 4,1);
+<<<<<<< Upstream, based on origin/master
 <<<<<<< Upstream, based on origin/fraborcar
 >>>>>>> 44dd997 Añadido horas al pdf factura
 <<<<<<< Upstream, based on origin/master
@@ -256,10 +251,13 @@ INSERT INTO linea_factura(precio_base, descuento, reparacion, descripcion, recam
 >>>>>>> 9e1deb7 Merge
 >>>>>>> 1b96e9a Merge
 >>>>>>> 9f5e1df Merge
+=======
+>>>>>>> 629ca62 merge
 
 INSERT INTO horas_trabajadas(id, trabajo_realizado, precio_hora, horas_trabajadas, empleado) VALUES (10, 'Cambio de rueda', 10.5, 3, 100);
 INSERT INTO horas_trabajadas(id, trabajo_realizado, precio_hora, horas_trabajadas, empleado) VALUES (11, 'Cambio de frenos', 10.5, 4, 101);
 
+<<<<<<< Upstream, based on origin/master
 <<<<<<< Upstream, based on origin/fraborcar
 <<<<<<< Upstream, based on origin/fraborcar
 INSERT INTO horas_reparacion VALUES (1, 10);
@@ -283,8 +281,11 @@ INSERT INTO reparaciones_empleados(REPARACION_ID, EMPLEADOS_ID) VALUES (1, 101);
 =======
 <<<<<<< Upstream, based on origin/master
 >>>>>>> 1b96e9a Merge
+=======
+>>>>>>> 629ca62 merge
 INSERT INTO horas_reparacion VALUES (2, 10);
 INSERT INTO horas_reparacion VALUES (2, 11);
+<<<<<<< Upstream, based on origin/master
 <<<<<<< Upstream, based on origin/fraborcar
 >>>>>>> 44dd997 Añadido horas al pdf factura
 <<<<<<< Upstream, based on origin/master
@@ -310,6 +311,8 @@ INSERT INTO reparaciones(id, descripcion, tiempo_estimado, fecha_finalizacion, f
 >>>>>>> 9e1deb7 Merge
 >>>>>>> 1b96e9a Merge
 >>>>>>> 9f5e1df Merge
+=======
+>>>>>>> 629ca62 merge
 
 INSERT INTO reparacionescomunes(id, nombre, descripcion) VALUES (1, 'Reparación espejo retrovisor izquierdo', 'Po lo hase así y asá');
 INSERT INTO reparacionescomunes(id, nombre, descripcion) VALUES (2, 'Reparación luneta térmica', 'Po lo hase así y con la mano asá');
@@ -325,5 +328,6 @@ INSERT INTO solicitudes(terminada, cantidad, recambio_id, empleado_id, reparacio
 
 
 -- 
+
 
 
