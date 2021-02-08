01b965986b2a37030c3a6e0bc86d85ccbafeadd1
DJMori <alemorgar4@alum.us.es>
2021-02-08 14:54:48 +0100

    merge

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -231,17 +231,10 @@ INSERT INTO reparacionescomunes(id, nombre, descripcion) VALUES (1, 'Reparación
 INSERT INTO reparacionescomunes(id, nombre, descripcion) VALUES (2, 'Reparación luneta térmica', 'Po lo hase así y con la mano asá');
 
 
-<<<<<<< Upstream, based on origin/master
-INSERT INTO pedidos(id, name, cantidad, precio_unidad, recibido, proveedor_id) VALUES (1, 'Batería INNPO Lcpower 74Ah 640A', 2, 175.02, true, 201);
-INSERT INTO pedidos(id, name, cantidad, precio_unidad, recibido, proveedor_id) VALUES (2, 'Tubo de escape H83LS', 1, 214.32, true, 201);
-=======
+
 
 INSERT INTO pedidos(id, cantidad, precio_unidad, recibido, proveedor_id, recambio) VALUES (1, 3, 214.32, false, 201, 100);
 INSERT INTO pedidos(id, cantidad, precio_unidad, recibido, proveedor_id, recambio) VALUES (2, 3, 214.32, true, 201, 101);
-<<<<<<< HEAD
->>>>>>> a33de81 Arreglado relacion pedidoRecambio-Recambio
-=======
->>>>>>> branch 'alemorgar4' of https://github.com/gii-is-DP1/dp1-2020-g2-01
 
 
 -- Pruebas con solicitudes
